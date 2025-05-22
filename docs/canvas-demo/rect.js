fa28930876bc797b392d598b26218a386aebeefa
cod10129 <110200933+cod10129@users.noreply.github.com>
2025-05-22 09:25:43 -0500

    add Rect intersection detection functions

--- docs/canvas-demo/rect.js
+++ docs/canvas-demo/rect.js
@@ -3,7 +3,7 @@ class Rect {
     y;
     width;
     height;
-    
+
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
@@ -14,4 +14,42 @@ class Rect {
     get right() { return this.x + this.width }
     get top() { return this.y }
     get bottom() { return this.y + this.height }
+    center() {
+        return new Vec2(
+            this.x + this.width / 2,
+            this.y + this.height / 2,
+        )
+    }
+
+    /**
+     * Does this `Rect` intersect, at all, the `other` one?
+     */
+    intersectRect(other) {
+        // Borrowed from https://docs.rs/kurbo/0.11.2/src/kurbo/rect.rs.html#273-275
+        (this.left <= other.right)
+            && (this.right >= other.left)
+            && (this.top <= other.bottom)
+            && (this.bottom >= other.top)
+    }
+
+    /**
+     * Does this `Rect` intersect the circle given by `pos` and `radius`?
+     */
+    intersectCircle(pos, radius) {
+        // https://stackoverflow.com/a/402010/21760104
+        const center = this.center();
+        const circleDistanceX = Math.abs(pos.x - center.x);
+        const circleDistanceY = Math.abs(pos.y - center.y);
+
+        if (circleDistanceX > (this.width /2 + radius)) { return false }
+        if (circleDistanceY > (this.height/2 + radius)) { return false }
+
+        if (circleDistanceX <= (this.width  / 2)) { return true }
+        if (circleDistanceY <= (this.height / 2)) { return true }
+
+        const cornerDistanceSquared =
+            (circleDistanceX - (this.width  / 2))^2 +
+            (circleDistanceY - (this.height / 2))^2;
+        return (cornerDistanceSquared <= (radius^2));
+    }
 }
